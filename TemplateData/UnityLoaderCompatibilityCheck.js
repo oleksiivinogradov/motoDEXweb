20729b221cfc9ca1deacab9a7ac8e91ec2c99f99
EVIL\filis <filisteev1999@gmail.com>
2023-03-20 17:32:33 +0100

    New version

--- TemplateData/UnityLoaderCompatibilityCheck.js
+++ TemplateData/UnityLoaderCompatibilityCheck.js
@@ -1,7 +1,10 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 UnityLoader.compatibilityCheck = function (unityInstance, onsuccess, onerror) {
-  if (!UnityLoader.SystemInfo.hasWebGL) {
-    unityInstance.popup('Your browser does not support WebGL',
-      [{text: 'OK', callback: onerror}]);
+  if (isMobile) {
+    unityInstance.popup('This game is not optimized for mobile devices', [{text: 'OK', callback: onerror}]);
+  } else if (!UnityLoader.SystemInfo.hasWebGL) {
+    unityInstance.popup('Your browser does not support WebGL', [{text: 'OK', callback: onerror}]);
   } else {
     onsuccess();
   }
